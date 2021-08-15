f9a6383f99c58a2a232ea3afc592567576d2ab35
Abolfazl Mokhtari <abolfazl021mokhtari@gmail.com>
2021-08-15 18:04:08 +0430

    create navbar and section1 home page

--- src/Container/App.jsx
+++ src/Container/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Nav from '../Component/Nav/Nav';
 const App = () => {
   return ( 
     <div>
-      <h1 className="text-blue-500">abolfazl</h1>
+      <Nav/>
+      <div className="pt-20">
+      <img src="image/Web.png" className="w-2/3 m-auto " alt="logo" />
+      <h1 className="text-xl text-center mt-5">آموزشگاه آنلاین <span style={{color:"#ffbb00"}}>پونس</span></h1>
+      <p className="text-center text-gray-700 mt-3">از صفر تا ورود به بازار کار با شما  هستیم</p>
+      </div>
+
+      
     </div>
    );
 }
