cb4bd68b7543d8b8fda5f6acf1917bf42620c512
Abolfazl Mokhtari <abolfazl021mokhtari@gmail.com>
2021-08-21 11:32:51 +0430

    responsive section1 homepage

--- src/Component/HomePage/Section1.jsx
+++ src/Component/HomePage/Section1.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Section1 = () => {
     return (
-        <div className="pt-20">
-            <img src="image/Web.png" className="w-2/3 m-auto " alt="logo" />
-            <h1 className="text-xl text-center mt-5">آموزشگاه آنلاین <span style={{ color: "#ffbb00" }}>پونس</span></h1>
-            <p className="text-center text-gray-700 mt-3">از صفر تا ورود به بازار کار با شما  هستیم</p>
+        <div className="pt-20 md:flex">
+            <img src="image/Web.png" className="w-2/3 m-auto md:w-1/4" alt="logo" />
+            <div className="md:mx-auto md:my-auto ">
+            <h1 className="text-xl text-center mt-5 md:text-4xl">آموزشگاه آنلاین <span style={{ color: "#ffbb00" }}>پونس</span></h1>
+            <p className="text-center text-gray-700 mt-3 md:text-2xl">از صفر تا ورود به بازار کار با شما  هستیم</p>
+            </div>
         </div>
     );
 }
